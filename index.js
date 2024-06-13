f94b3cfd3a6dfdf1dc26af73e333927eb4454bae
Iryna Burlaka <i.am.blinova@gmail.com>
2024-06-13 15:32:05 +0200

    create function

---
+++ index.js
@@ -0,0 +1,7 @@
+const alfabet = document.querySelector(".alfabet");
+
+const phrase = "Beggars can not be choosers";
+
+function createPassword() {
+  document.querySelector(".password").innerHTML = phrase;
+}
